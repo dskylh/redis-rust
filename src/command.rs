a34fcd00b5c20fd565b48065b60d86facef2306a
Taha Ugurcan Ongel <dskylh@protonmail.com>
2024-02-22 20:16:38 +0300

    4

--- src/command.rs
+++ src/command.rs
@@ -2,6 +2,7 @@ use bytes::{Bytes, BytesMut};
 
 use crate::parser::{RedisEncoder, RedisValueRef};
 
+#[derive(Debug, PartialEq)]
 pub enum RespCommand {
     Ping,
     Echo(Bytes),
@@ -30,11 +31,7 @@ impl RespCommand {
     }
 
     fn ping(&self) -> Bytes {
-        let mut encoder = RedisEncoder::default();
-        let value = RedisValueRef::String("+PONG".as_bytes().into());
-        let mut buf = BytesMut::new();
-        encoder.encode(value, &mut buf);
-        buf.into()
+        BytesMut::from("+PONG\r\n").into()
     }
 
     fn echo(&self, msg: &Bytes) -> Bytes {
@@ -43,5 +40,30 @@ impl RespCommand {
         let mut buf = BytesMut::new();
         encoder.encode(value, &mut buf);
         buf.into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_command() {
+        let command = RespCommand::parse_command("ping");
+        assert_eq!(command, RespCommand::Ping);
+
+        let command = RespCommand::parse_command("echo hello");
+        assert_eq!(command, RespCommand::Echo(Bytes::from("hello")));
+    }
+
+    #[test]
+    fn test_execute() {
+        let command = RespCommand::Ping;
+        let response = command.execute();
+        assert_eq!(response, Bytes::from("+PONG\r\n"));
+
+        let command = RespCommand::Echo(Bytes::from("hello"));
+        let response = command.execute();
+        assert_eq!(response, Bytes::from("$5\r\nhello\r\n"));
     }
 }
